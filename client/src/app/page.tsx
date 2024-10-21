f689d343d3da4bd39d07b4a61f239c03c2fc223c
Clément Prod'homme <contact@clementprodhomme.fr>
2024-10-21 11:51:03 +0200

    Store the map's bounds in the URL

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,16 @@
+import { Metadata } from "next";
 import dynamic from "next/dynamic";
 
 // By forcing the map to load in the client, we can perform some media queries immediately. Without
 // this, the map would still be loaded in the client only anyway.
 const Map = dynamic(() => import("@/components/map"), { ssr: false });
+
+export const metadata: Metadata = {
+  alternates: {
+    // Make sure that the query strings can be ignored by search engines
+    canonical: "/",
+  },
+};
 
 export default function Home() {
   return (
--- client/src/components/map/index.tsx
+++ client/src/components/map/index.tsx
@@ -1,24 +1,36 @@
 "use client";
 
+import { useCallback, useRef } from "react";
 import ReactMapGL from "react-map-gl";
 
 import { env } from "@/env";
 import useBreakpoint from "@/hooks/use-breakpoint";
+import useMapBounds from "@/hooks/use-map-bounds";
 
-import { DEFAULT_BOUNDS, DESKTOP_MAX_BOUNDS, MOBILE_MAX_BOUNDS } from "./constants";
+import { DESKTOP_MAX_BOUNDS, MOBILE_MAX_BOUNDS } from "./constants";
+
+import type { MapRef, LngLatLike } from "react-map-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const Map = () => {
   const isDesktop = useBreakpoint("xl", false, true);
+  const mapRef = useRef<MapRef>(null);
 
-  console.log(isDesktop);
+  const [bounds, setBounds] = useMapBounds();
+
+  const onMove = useCallback(() => {
+    if (mapRef.current) {
+      setBounds(mapRef.current.getBounds()?.toArray() as [LngLatLike, LngLatLike]);
+    }
+  }, [mapRef, setBounds]);
 
   return (
     <ReactMapGL
+      ref={mapRef}
       mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_TOKEN}
       initialViewState={{
-        bounds: DEFAULT_BOUNDS,
+        bounds: bounds,
         fitBoundsOptions: {
           padding: isDesktop ? 100 : 20,
         },
@@ -26,6 +38,7 @@ const Map = () => {
       maxBounds={isDesktop ? DESKTOP_MAX_BOUNDS : MOBILE_MAX_BOUNDS}
       style={{ width: "100%", height: "100%" }}
       mapStyle={env.NEXT_PUBLIC_MAPBOX_STYLE}
+      onMove={onMove}
     />
   );
 };
