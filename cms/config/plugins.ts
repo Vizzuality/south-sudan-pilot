12f6151a593f05b24d1dde028387d5a0afc8ff78
Kevin <kew.sanchez@gmail.com>
2024-10-23 18:28:40 +0200

    feat(Email): Adds configuration for Strapi's email service

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+export default ({env}) => ({
+  email: {
+    config: {
+      provider: 'amazon-ses',
+      providerOptions: {
+        key: env('AWS_SES_ACCESS_KEY_ID'),
+        secret: env('AWS_SES_ACCESS_KEY_SECRET'),
+        amazon: 'https://email.af-south-1.amazonaws.com',
+      },
+      settings: {
+        defaultFrom: 'strapi@ss-hydro-pilot.gmv.com',
+        defaultReplyTo: 'strapi@ss-hydro-pilot.gmv.com',
+      },
+    },
+  }
+
+});
