8408b74a0f85f608b8ab7d2a6fd53482bb38de06
Clément Prod'homme <contact@clementprodhomme.fr>
2024-10-28 15:41:53 +0100

    feat(client): Display toggles for contextual layers

--- client/src/components/panels/contextual-layers/item.tsx
+++ client/src/components/panels/contextual-layers/item.tsx
@@ -1,3 +1,5 @@
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 import { Dataset, Layer } from "@/types/generated/strapi.schemas";
 
 interface ItemProps {
@@ -11,8 +13,13 @@ const Item = ({ name, layers }: ItemProps) => {
       <div className="border-b border-casper-blue-400/50 py-2 uppercase">{name}</div>
       <ul className="py-2">
         {layers.map((layer) => (
-          <li key={layer.id} className="py-2">
-            <span className="text-xl">{layer.name}</span>
+          <li key={layer.id} className="flex items-start justify-between gap-4 py-2">
+            <Label htmlFor={`${layer.id}-toggle`} className="text-xl">
+              {layer.name}
+            </Label>
+            <div className="pt-1">
+              <Switch id={`${layer.id}-toggle`} />
+            </div>
           </li>
         ))}
       </ul>
