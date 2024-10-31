af7164761855dcbd1d4c7a7d36edc2d3bf38471a
Clément Prod'homme <contact@clementprodhomme.fr>
2024-10-31 13:27:02 +0100

    fix(client): Style of the Switch

--- client/src/components/ui/switch.tsx
+++ client/src/components/ui/switch.tsx
@@ -19,7 +19,7 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-5 w-5 rounded-full border-[3px] border-white bg-rhino-blue-950 ring-0 transition-transform data-[state=checked]:translate-x-[16px] data-[state=unchecked]:-translate-x-[3px]",
+        "pointer-events-none block h-5 w-5 rounded-full border-[3px] border-white bg-rhino-blue-950 ring-0 transition-transform data-[state=checked]:translate-x-3 data-[state=unchecked]:-translate-x-[3px]",
       )}
     />
   </SwitchPrimitives.Root>
